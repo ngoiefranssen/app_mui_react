155f7d11ea6f9109de39b394e897aa3578a2b10f
Franssen <ngoiefranssen04@gmail.com>
2023-01-16 16:43:27 +0200

    Wip

--- src/Components/Controls/ActionButton.jsx
+++ src/Components/Controls/ActionButton.jsx
@@ -2,24 +2,24 @@ import { Button } from '@mui/material'
 import { minHeight, style } from '@mui/system'
 import React from 'react'
 
-const useStyles = style((theme) =>{
-    root:{
-        minHeight: 0,
-        margin: theme.spacin(0.5),
-    }
-    secondary:{
-        backgroundColor: theme.palette.secondary.light
-        '& .MuiButton-label' :{
-            color: theme.palette.secondary.main
-        }
-    }
-    primary:{
-        backgroundColor: theme.palette.primary.light
-        '& .MuiButton-label' :{
-            color : theme.palette.primary.main
-        }
-    }
-})
+// const useStyles = style((theme) =>{
+//     root:{
+//         minHeight: 0,
+//         margin: theme.spacin(0.5),
+//     }
+//     secondary:{
+//         backgroundColor: theme.palette.secondary.light
+//         '& .MuiButton-label' :{
+//             color: theme.palette.secondary.main
+//         }
+//     }
+//     primary:{
+//         backgroundColor: theme.palette.primary.light
+//         '& .MuiButton-label' :{
+//             color : theme.palette.primary.main
+//         }
+//     }
+// })
 
 export default function ActionButton(props) {
     const { color, children, onClick } = props
