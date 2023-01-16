3d2d4ea61f7c5380a0a859c1f04b4e26519feec0
Franssen <ngoiefranssen04@gmail.com>
2023-01-16 16:29:56 +0200

    Wip

--- src/Components/Controls/ActionButton.jsx
+++ src/Components/Controls/ActionButton.jsx
@@ -1,9 +1,24 @@
 import { Button } from '@mui/material'
-import { style } from '@mui/system'
+import { minHeight, style } from '@mui/system'
 import React from 'react'
 
 const useStyles = style((theme) =>{
-
+    root:{
+        minHeight: 0,
+        margin: theme.spacin(0.5),
+    }
+    secondary:{
+        backgroundColor: theme.palette.secondary.light
+        '& .MuiButton-label' :{
+            color: theme.palette.secondary.main
+        }
+    }
+    primary:{
+        backgroundColor: theme.palette.primary.light
+        '& .MuiButton-label' :{
+            color : theme.palette.primary.main
+        }
+    }
 })
 
 export default function ActionButton(props) {
