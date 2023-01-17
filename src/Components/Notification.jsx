aeedf91f4762d8886669f1ca5ef2dcb43dc4594e
Franssen <ngoiefranssen04@gmail.com>
2023-01-17 10:46:52 +0200

    Wip

--- src/Components/Notification.jsx
+++ src/Components/Notification.jsx
@@ -3,10 +3,20 @@ import React from 'react'
 
 const Notification = () => {
     const [notify, setNotify] = React.useState()
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setNotify({
+            ...notify,
+            isOpen: false
+        })
+    }
     return (
         <Snackbar
         open={notify.isOpen}
         autoHideDuration={4000}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
         >
             <Alert>
                 {notify.message}
